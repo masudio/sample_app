ce302d2ec5b60b47853a609deb81f7e8d76fcfcc
Masud Khan <masudio@gmail.com>
2011-12-27 11:30:04 -0700

    removed index.html and now routes to pages/home.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  
   get "pages/home"
 
   get "pages/contact"
@@ -56,7 +60,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
